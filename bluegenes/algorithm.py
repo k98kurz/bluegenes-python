a1f6963db40432ff81d0381794aa34c4b1e219e4
Jon Tyler <k98kurz@gmail.com>
2023-07-11 18:30:29 -0400

    ditto for Chromosome optimization

--- bluegenes/algorithm.py
+++ bluegenes/algorithm.py
@@ -148,3 +148,79 @@ def allele_child_from_parents(parents: list[Allele]) -> Allele:
     weights = [len(parents[i:])/len(parents) for i in range(len(parents))]
     dad, mom = choices(parents, weights, k=2)
     return dad.recombine(mom)
+
+
+def optimize_chromosome(
+        measure_fitness: Callable[[Chromosome, int|float], int|float],
+        mutate_chromosome: Callable[[Chromosome], Chromosome],
+        initial_population: list[Chromosome] = None, population_size: int = 100,
+        chromosome_size: int = 2, allele_size: int = 3, gene_size: int = 10,
+        fitness_target: int|float = 1.0, max_iterations: int = 1000,
+        base_factory: Callable[[Any], int|float|str] = None,
+        factory_args: list[Any] = None,
+        factory_kwargs: dict[str, Any] = None,
+        chromosome_name: str = None, parents_per_generation: int = 10,
+    ) -> tuple[int, list[Chromosome]]:
+    """Optimize an Chromosome given a measure_fitness function, a
+        mutate_chromosome function, a population_size int, a fitness_target
+        float, and a max_iterations int. Supply base_factory to produce
+        Gene bases other than random ints between 0 and 10,
+        with optional factory_args and factory_kwargs which will be
+        passed to each call of base_factory. Supply chromosome_name to
+        assign the name to each generated Chromosome in the population.
+        Supply an allele_size int and a gene_size int to customize
+        generation of a random initial population, or supply
+        initial_population list[Chromosome] to specify the initial
+        population. Returns the number of iterations and the final
+        population.
+    """
+    tert(initial_population is None or all(type(p) is Chromosome for p in initial_population),
+         "initial_population must be None or list[Chromosome]")
+    population = [
+        Chromosome.make(
+            chromosome_size, allele_size, gene_size, base_factory=base_factory,
+            factory_args=factory_args, factory_kwargs=factory_kwargs,
+            name=chromosome_name
+        )
+        for _ in range(population_size)
+    ] if initial_population is None else initial_population
+
+    count = 0
+    fitness_scores: list[tuple[int|float, Gene]] = [
+        (measure_fitness(g, fitness_target), g)
+        for g in population
+    ]
+    fitness_scores.sort(key=lambda fs: fs[0])
+    fitness_scores.reverse()
+    best_fitness = fitness_scores[0][0]
+
+    while count < max_iterations and best_fitness < fitness_target:
+        # breed parents at random proportional to their order by score
+        parents = [fs[1] for fs in fitness_scores[:parents_per_generation]]
+        children = [
+            chromosome_child_from_parents(parents)
+            for _ in range(population_size-len(parents))
+        ]
+
+        children = [mutate_chromosome(child) for child in children]
+        population = [*children, *parents]
+        fitness_scores: list[tuple[int|float, Gene]] = [
+            (measure_fitness(g, fitness_target), g)
+            for g in population
+        ]
+        fitness_scores.sort(key=lambda fs: fs[0])
+        fitness_scores.reverse()
+        best_fitness = fitness_scores[0][0]
+        count += 1
+
+    population = [fs[1] for fs in fitness_scores]
+    return (count, population)
+
+def chromosome_child_from_parents(parents: list[Chromosome]) -> Chromosome:
+    """Select two parents at random semi-proportional to their order in
+        the list. Recombine the two chosen parent Genes, and return the
+        result.
+    """
+    weights = [len(parents[i:])/len(parents) for i in range(len(parents))]
+    dad, mom = choices(parents, weights, k=2)
+    return dad.recombine(mom)
--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -154,5 +154,125 @@ class TestAlgorithmForAlleles(unittest.TestCase):
         assert count < 1000 or (best[0] - target)/target < 0.01
 
 
+class TestAlgorithmForChromosomes(unittest.TestCase):
+    def population(self) -> list[genes.Chromosome]:
+        return [
+            genes.Chromosome("test", [
+                genes.Allele("al1", [
+                    genes.Gene("gn1", [1.0, 2.0, 3.0, 4.0]),
+                    genes.Gene("gn2", [2.1, 1.1, 4.1, 3.1]),
+                ]),
+                genes.Allele("al2", [
+                    genes.Gene("gn1", [11.0, 12.0, 13.0, 14.0]),
+                    genes.Gene("gn2", [12.1, 11.1, 14.1, 13.1]),
+                ]),
+            ]),
+            genes.Chromosome("test", [
+                genes.Allele("al1", [
+                    genes.Gene("gn1", [1.2, 2.2, 3.2, 4.2]),
+                    genes.Gene("gn2", [2.3, 1.3, 4.3, 3.3]),
+                ]),
+                genes.Allele("al2", [
+                    genes.Gene("gn1", [11.2, 12.2, 13.2, 14.2]),
+                    genes.Gene("gn2", [12.3, 11.3, 14.3, 13.3]),
+                ]),
+            ]),
+            genes.Chromosome("test", [
+                genes.Allele("al1", [
+                    genes.Gene("gn1", [1.4, 2.4, 3.4, 4.4]),
+                    genes.Gene("gn2", [2.5, 1.5, 4.5, 3.5]),
+                ]),
+                genes.Allele("al2", [
+                    genes.Gene("gn1", [11.4, 12.4, 13.4, 14.4]),
+                    genes.Gene("gn2", [12.5, 11.5, 14.5, 13.5]),
+                ]),
+            ]),
+            genes.Chromosome("test", [
+                genes.Allele("al1", [
+                    genes.Gene("gn1", [1.6, 2.6, 3.6, 4.6]),
+                    genes.Gene("gn2", [2.7, 1.7, 4.7, 3.7]),
+                ]),
+                genes.Allele("al2", [
+                    genes.Gene("gn1", [11.6, 12.6, 13.6, 14.6]),
+                    genes.Gene("gn2", [12.7, 11.7, 14.7, 13.7]),
+                ]),
+            ]),
+        ]
+
+    def setUp(self) -> None:
+        self.parents = self.population()
+        return super().setUp()
+
+    def test_chromosome_child_from_parents_returns_recombination_of_two_random_parents(self):
+        child = algorithm.chromosome_child_from_parents(self.parents)
+
+        parent_alleles = {
+            i: self.parents[i].alleles
+            for i in range(len(self.parents))
+        }
+        parent_genes = {
+            i: [g for a in pa for g in a.genes]
+            for i, pa in parent_alleles.items()
+        }
+        parent_bases = {
+            i: [b for g in pg for b in g.bases]
+            for i, pg in parent_genes.items()
+        }
+        parents = set()
+        for allele in child.alleles:
+            for gene in allele.genes:
+                for base in gene.bases:
+                    for k, v in parent_bases.items():
+                        if base in v:
+                            parents.add(k)
+        assert len(parents) <= 2
+
+    def test_optimize_chromosome_exits_after_max_iterations_or_achieving_fitness_target(self):
+        target = 123.456
+
+        def measure_fitness(chromosome: genes.Chromosome, target: float) -> float:
+            sums = [sum(g.bases) for a in chromosome.alleles for g in a.genes]
+            return 1 / abs(sum(sums) - target)
+
+        def mutate_gene(gene: genes.Gene) -> genes.Gene:
+            for i in range(len(gene.bases)):
+                val = random()
+                if val <= 0.1:
+                    gene.bases[i] *= random()
+                elif val <= 0.2:
+                    gene.bases[i] /= random()
+                elif val <= 0.6:
+                    gene.bases[i] += random()
+                else:
+                    gene.bases[i] -= random()
+            return gene
+
+        def mutate_allele(allele: genes.Allele) -> genes.Allele:
+            allele.genes = [mutate_gene(g) for g in allele.genes]
+            return allele
+
+        def mutate_chromosome(chromosome: genes.Chromosome) -> genes.Chromosome:
+            chromosome.alleles = [mutate_allele(a) for a in chromosome.alleles]
+            return chromosome
+
+        count, population = algorithm.optimize_chromosome(
+            measure_fitness=measure_fitness,
+            mutate_chromosome=mutate_chromosome,
+            initial_population=self.population(),
+            fitness_target=target,
+            max_iterations=1000,
+            population_size=100
+        )
+
+        assert type(count) is int and count <= 1000
+        assert type(population) is list
+        assert all(type(p) is genes.Chromosome for p in population)
+        assert len(population) == 100
+        sums = [sum(g.bases) for a in population[0].alleles for g in a.genes]
+        best = (sum(sums), population[0])
+
+        assert count < 1000 or (best[0] - target)/target < 0.01
+
+
 if __name__ == '__main__':
     unittest.main()
