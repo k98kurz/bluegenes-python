1c2989084e581c172c63d2a1835a6dfe7476df3f
Jon Tyler <k98kurz@gmail.com>
2023-07-11 19:27:13 -0400

    __init__.py

---
+++ bluegenes/__init__.py
@@ -0,0 +1,12 @@
+from .genes import (
+    Gene,
+    Allele,
+    Chromosome,
+    Genome
+)
+from .optimization import (
+    optimize_gene,
+    optimize_allele,
+    optimize_chromosome,
+    optimize_genome
+)
